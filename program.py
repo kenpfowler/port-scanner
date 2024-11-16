0dda838f2032a41a73bba53002316443f929554c
Ken Fowler <development@kennyf.ca>
2024-11-16 16:51:31 -0500

    refactor: update to argparse lib

--- program.py
+++ program.py
@@ -1,41 +1,53 @@
-import getopt, sys
+import argparse
+
 from PortScanner import PortScanner
 
-# Remove 1st argument from the
-# list of command line arguments
-argument_list = sys.argv[1:]
 
-# Options
-options = "hp:"
-
-# Long options
-long_options = ["host=", "port="]
-
-host = "localhost"
-port = 8080
-
-try:
-    # Parsing argument
-    arguments, values = getopt.getopt(argument_list, options, long_options)
-    
-    # checking each argument
-    for current_argument, current_value in arguments:
-
-        if current_argument in ("-h", "--host"):
-            host = current_value
-            print (("specified host (% s)") % (current_value))
-            
-        elif current_argument in ("-p", "--port"):
-            port = int(current_value)
-            print (("specified port (% s)") % (current_value))
-            
-except getopt.error as err:
-    # output error, and return with an error code
-    print (str(err))
+def validate_ip_range(ip_range):
+    """Validate and parse an IP address range in the format 'start_ip-end_ip'."""
+    try:
+        start_ip, end_ip = ip_range.split("-")
+        # Optionally, you could validate the IP addresses using socket or a regex
+        return start_ip, end_ip
+    except ValueError:
+        raise argparse.ArgumentTypeError(
+            "IP range must be in the format 'start_ip-end_ip'."
+        )
 
 
-port_scanner = PortScanner(host, port)
+def main():
+    # Create the argument parser
+    parser = argparse.ArgumentParser(description="Scan for open ports on a device.")
 
-port_scanner.start()
+    # TODO: support an ip range feature
+    # Add argument for IP range
+    # parser.add_argument(
+    #     "ip_range",
+    #     type=validate_ip_range,
+    #     help="Specify the range of IP addresses in the format 'start_ip-end_ip'."
+    # )
+
+    # Add optional argument for specific ports or port ranges
+    parser.add_argument(
+        "-p",
+        "--ports",
+        type=str,
+        default="1-65535",  # Default to all ports
+        help="Specify port(s) to scan, e.g., '80', '20-25', or '80,443,8080'.",
+    )
+
+    # Parse the arguments
+    args = parser.parse_args()
+
+    # Extract the parsed arguments
+    # TODO: support an ip range feature
+    # start_ip, end_ip = args.ip_range
+    ports = args.ports
+
+    # Add logic for scanning IPs and ports here...
+    port_scanner = PortScanner("localhost", ports)
+    port_scanner.start()
 
 
+if __name__ == "__main__":
+    main()
